eb5ea9b45fcfe7467d39b08596ba08b76c6609bc
souhmaa <maalaoui.ensi@gmail.com>
2018-09-24 23:24:25 +0200

    Implement solution

--- src/main/java/eu/els/tdd/calculator/StringCalculator.java
+++ src/main/java/eu/els/tdd/calculator/StringCalculator.java
@@ -1,5 +1,29 @@
 package eu.els.tdd.calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
+
+    public int add(String numbers) {
+        if (isNullOrEmpty(numbers)) {
+            return 0;
+        }
+        return splitAndCalculateSum(numbers);
+    }
+
+    private boolean isNullOrEmpty(String numbers) {
+        return numbers == null || numbers.isEmpty();
+    }
+
+    private int splitAndCalculateSum(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .filter(this::isNumeric)
+                .map(Integer::valueOf)
+                .reduce(0, (nbr1, nbr2) -> nbr1 + nbr2);
+    }
+
+    private boolean isNumeric(String str) {
+        return str.chars().allMatch(Character::isDigit);
+    }
 
 }
--- src/test/java/eu/els/tdd/calculator/StringCalculatorTest.java
+++ src/test/java/eu/els/tdd/calculator/StringCalculatorTest.java
@@ -1,4 +1,82 @@
 package eu.els.tdd.calculator;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
+
+
+    private StringCalculator calculator;
+
+    @Before
+    public void setUp() {
+        this.calculator = new StringCalculator();
+    }
+
+    @Test
+    public void
+    GIVEN_numbers_is_null_WHEN_add_THEN_sum_is_0() {
+        // GIVEN
+        String numbers = null;
+
+        // WHEN
+        int sum = this.calculator.add(numbers);
+
+        //THEN
+        assertEquals(0, sum);
+    }
+
+    @Test
+    public void given_numbers_is_empty_WHEN_add_THEN_sum_is_0() {
+        // GIVEN
+        String numbers = "";
+
+        // WHEN
+        int sum = this.calculator.add(numbers);
+
+        //THEN
+        assertEquals(0, sum);
+    }
+
+    @Test
+    public void
+    GIVEN_number_is_1_comma_2_WHEN_add_THEN_sum_is_three() {
+        // GIVEN
+        String numbers = "1,2";
+
+        // WHEN
+        int sum = this.calculator.add(numbers);
+
+        //THEN
+        assertEquals(3, sum);
+    }
+
+    @Test
+    public void
+    GIVEN_number_is_1_WHEN_add_THEN_sum_is_1() {
+        // GIVEN
+        String numbers = "1";
+
+        // WHEN
+        int sum = this.calculator.add(numbers);
+
+        //THEN
+        assertEquals(1, sum);
+    }
+
+    @Test
+    public void
+    GIVEN_numbers_contains_invalid_number_WHEN_add_THEN_ignore_invalid_numbers() {
+        // GIVEN
+        String numbers = "1,32,a,4,aza:,2";
+
+        // WHEN
+        int sum = this.calculator.add(numbers);
+
+        // THEN
+        assertEquals(39, sum);
+    }
+
 }
